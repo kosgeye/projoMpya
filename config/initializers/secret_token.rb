1bd0279612d08ea7175191de47cfb29c19ab9a7d
Eric K. Kosgey <kosgeye@berea.edu>
2014-02-17 16:16:14 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjoMpya::Application.config.secret_key_base = 'fe0e19457bbec901aac9919a7cb951c735ef07025cd74a4e4f7032803344ab7f0b35aa6a3a9485c5340149fccb4b4403a0bf61780895fe1da3682d0cdd4fede4'
